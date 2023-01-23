7a8360c77327c1d876c33945e0f24312a7de7948
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-23 13:55:16 +0300

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,1 +1,43 @@
 #!/usr/bin/env python3
+"""Simple pagination"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """tuple of size two containing a start index and an end index """
+    index_tuple = page_size * (page - 1), page * page_size
+    return index_tuple
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """paginate dataset"""
+
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        indexes = index_range(page, page_size)
+        try: 
+            data = self.dataset()
+            return data[indexes[0]: indexes[1]]
+        except IndexError:
+            return []
