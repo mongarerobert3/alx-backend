29e597ca1a1adbb6faa3032a1d748a10e1cc5801
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-31 22:50:26 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,15 +11,18 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+
 
 @app.route('/')
 def get_index() -> str:
     """Index page"""
     return render_template('1-index.html')
 
+
 if __name__ == '_main__':
-    app.run(host = '0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
