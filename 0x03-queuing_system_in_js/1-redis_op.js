e815568bbf7b98bebc5cf525e9381306b0f897d0
mongarerobert3 <mongarerobert3@gmail.com>
2023-02-27 21:52:24 +0300

    Node Redis client and async operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -10,3 +10,17 @@ client.on('error', (err) => {
 client.on('connect', () => {
     console.log('Redis client connected to the server');
 });
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+    client.get(schoolName, (err, reply) => {
+        console.log(reply);
+    });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
